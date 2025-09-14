919a0fd7fcd6cc13fe743a108c56b48613f55caf
emanestares <fsestares@up.edu.ph>
2025-09-14 16:39:33 +0800

    add: partial contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section>Contact</section>
+    <section className='c-space my-20'>
+      <h3 className='head-text'>Contact Me</h3>
+      <div className='relative min-h-screen flex items-center justify-center flex-col'>
+        <img src="" alt="terminal background" className='absolute inset-0 min-h-screen'/>
+        <div className='contact-container'>
+          <h3 className='head-text'>Let's Talk</h3>
+          <p className='text-lg text-white-600 mt-3'>If you are looking for someone to make your ideas into actual application for mobile, desktop, and web, I am more than ready to help you! I also offer tutoring services for computer science courses. Feel free to contact me through the form below.</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
